afcf9e15f9e5024e105ad2d8f3fab580af15dba8
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2018-10-03 16:13:13 +0800

    修改QRCODE的JS

--- assets/js/qrcode.js
+++ assets/js/qrcode.js
@@ -1,4 +1,19 @@
 function htmlEncode (value){
   return $('<div/>').text(value).html();
 }
-    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").text()) + "&chs=160x160&chld=L|0");
+    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content1").text()) + "&chs=160x160&chld=L|0");
+
+function htmlEncode (value){
+  return $('<div/>').text(value).html();
+}
+    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content2").text()) + "&chs=160x160&chld=L|0");
+
+function htmlEncode (value){
+  return $('<div/>').text(value).html();
+}
+    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content3").text()) + "&chs=160x160&chld=L|0");
+
+function htmlEncode (value){
+  return $('<div/>').text(value).html();
+}
+    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content4").text()) + "&chs=160x160&chld=L|0");  
