82cf9fedb5fd59d0b2d151bc6ec38a5e2d2e46ba
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2018-10-04 15:34:53 +0800

    修改QRCODE

--- assets/js/qrcode.js
+++ assets/js/qrcode.js
@@ -1,19 +1,19 @@
 function htmlEncode (value){
-  return $("URL1").text(value).html();
+  return $('<div/>').text(value).html();
 }
-    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content1").text()) + "&chs=160x160&chld=L|0");
+    $(".qr-code1").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content1").text()) + "&chs=160x160&chld=L|0");
 
 function htmlEncode (value){
-  return $("URL2").text(value).html();
+  return $('<div/>').text(value).html();
 }
-    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content2").text()) + "&chs=160x160&chld=L|0");
+    $(".qr-code2").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content2").text()) + "&chs=160x160&chld=L|0");
 
 function htmlEncode (value){
-  return $("URL3").text(value).html();
+  return $('<div/>').text(value).html();
 }
-    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content3").text()) + "&chs=160x160&chld=L|0");
+    $(".qr-code3").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content3").text()) + "&chs=160x160&chld=L|0");
 
 function htmlEncode (value){
-  return $("URL4").text(value).html();
+  return $('<div/>').text(value).html();
 }
-    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content4").text()) + "&chs=160x160&chld=L|0");
+    $(".qr-code4").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content4").text()) + "&chs=160x160&chld=L|0");
